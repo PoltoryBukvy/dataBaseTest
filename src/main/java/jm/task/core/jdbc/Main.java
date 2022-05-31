bd779b5e19e598f1fe0bfcd85ef012f3d543780d
PoltoryBukvy <102367161+PoltoryBukvy@users.noreply.github.com>
2022-05-31 19:57:02 +0300

    исправлены замечания 6

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -9,25 +9,31 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        //UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-        //userDaoJDBC.createUsersTable();
-        //userDaoJDBC.saveUser("Yasha", "Kotik", (byte) 8);
-        //userDaoJDBC.saveUser("Marusya", "Kisa", (byte) 6);
-        //userDaoJDBC.getAllUsers();
-        //userDaoJDBC.removeUserById(1);
-        //userDaoJDBC.dropUsersTable();
-        //UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
-        //userDaoHibernate.createUsersTable();
-        //userDaoHibernate.saveUser("Yasha", "Kotik", (byte) 8);
-        //userDaoHibernate.saveUser("Marusya", "Kisa", (byte) 6);
-        //userDaoHibernate.getAllUsers();
-        //userDaoHibernate.close();
-        //userDaoHibernate.removeUserById(1);
+    public static void main(String[] args) {
+
+        /* UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+        userDaoJDBC.createUsersTable();
+        userDaoJDBC.saveUser("Yasha", "Kotik", (byte) 8);
+        userDaoJDBC.saveUser("Marusya", "Kisa", (byte) 6);
+        for (User user : userDaoJDBC.getAllUsers()) {
+            System.out.println(user);
+        }
+        userDaoJDBC.removeUserById(1);
+        userDaoJDBC.dropUsersTable(); */
+
+        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+        userDaoHibernate.createUsersTable();
+        userDaoHibernate.saveUser("Yasha", "Kotik", (byte) 8);
+        userDaoHibernate.saveUser("Marusya", "Kisa", (byte) 6);
+        for (User user : userDaoHibernate.getAllUsers()) {
+            System.out.println(user);
+        }
+
     }
 }
 
